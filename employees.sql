0127eb78224bda50fc82854215215b403297c995
Sharlleen <142578174+Sharlleen@users.noreply.github.com>
2023-08-19 01:29:54 +0800

    employees.sql

---
+++ employees.sql
@@ -0,0 +1,34 @@
+CREATE TABLE employees (
+    id INT,
+    name VARCHAR2(20),
+    position VARCHAR2(15),
+    department VARCHAR2(15),
+    salary NUMERIC,
+    PRIMARY KEY (id)
+);
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (100, 'John Doe', 'Manager', 'Sales', 58000);
+
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (200, 'Jane Smith', 'Assistant', 'Marketing', 35000);
+
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (300, 'Michael Johnson', 'Developer', 'IT', 60000);
+
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (400, 'Emily Brown', 'Analyst', 'Finance', 45000);
+
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (500, 'David Wilson', 'Clerk', 'HR', 30000);
+
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (600, 'Davidson', 'Clerk', 'HR', 30000);
+
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (700, 'Dason', 'Clerk', 'HR', 30000);
+
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (800, 'Liw', 'Analyst', 'Finance', 40000);
+
+INSERT INTO employees (id, name, position, department, salary)
+VALUES (900, 'Jooe', 'Manager', 'Sales', 58000);
